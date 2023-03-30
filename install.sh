9d3d760b62639919af0ef0734343cc8d4f097d60
Kroese <kroese@users.noreply.github.com>
2023-03-30 03:23:54 +0200

    Indentation

--- install.sh
+++ install.sh
@@ -8,89 +8,89 @@ IMG="/storage"
 [ ! -f "$IMG/boot.img" ] && rm -f $IMG/system.img
 [ -f "$IMG/system.img" ] && exit 0
 
-    echo "Downloading $URL..."
+echo "Downloading $URL..."
 
-    TMP="$IMG/tmp"
-    FILE="$TMP/dsm.pat"
+TMP="$IMG/tmp"
+FILE="$TMP/dsm.pat"
 
-    rm -rf $TMP && mkdir -p $TMP
-    wget $URL -O $FILE -q --show-progress
+rm -rf $TMP && mkdir -p $TMP
+wget $URL -O $FILE -q --show-progress
 
-    [ ! -f "$FILE" ] && echo "Download failed" && exit 61
+[ ! -f "$FILE" ] && echo "Download failed" && exit 61
 
-    SIZE=$(stat -c%s "$FILE")
+SIZE=$(stat -c%s "$FILE")
 
-    if ((SIZE<250000000)); then
-      echo "Invalid PAT file: File is an update pack which contains no OS image." && exit 62
-    fi
+if ((SIZE<250000000)); then
+  echo "Invalid PAT file: File is an update pack which contains no OS image." && exit 62
+fi
 
-    echo "Extracting downloaded system image..."
+echo "Extracting downloaded system image..."
 
-    if { tar tf "$FILE"; } >/dev/null 2>&1; then
-       tar xpf $FILE -C $TMP/.
-    else
-       export LD_LIBRARY_PATH="/run/extract"
-       if ! /run/extract/syno_extract_system_patch $FILE $TMP/. ; then
-         echo "Invalid PAT file: File is an update pack which contains no OS image." && exit 63
-       fi
-       export LD_LIBRARY_PATH=""
-    fi
+if { tar tf "$FILE"; } >/dev/null 2>&1; then
+   tar xpf $FILE -C $TMP/.
+else
+   export LD_LIBRARY_PATH="/run/extract"
+   if ! /run/extract/syno_extract_system_patch $FILE $TMP/. ; then
+     echo "Invalid PAT file: File is an update pack which contains no OS image." && exit 63
+   fi
+   export LD_LIBRARY_PATH=""
+fi
 
-    HDA="$TMP/hda1"
-    IDB="$TMP/indexdb"
-    HDP="$TMP/synohdpack_img"
+HDA="$TMP/hda1"
+IDB="$TMP/indexdb"
+HDP="$TMP/synohdpack_img"
 
-    [ ! -f "$HDA.tgz" ] && echo "Invalid PAT file: File contains no OS image." && exit 64
-    [ ! -f "$HDP.txz" ] && echo "Invalid PAT file: HD pack not found." && exit 65
-    [ ! -f "$IDB.txz" ] && echo "Invalid PAT file: IndexDB file not found." && exit 66
+[ ! -f "$HDA.tgz" ] && echo "Invalid PAT file: File contains no OS image." && exit 64
+[ ! -f "$HDP.txz" ] && echo "Invalid PAT file: HD pack not found." && exit 65
+[ ! -f "$IDB.txz" ] && echo "Invalid PAT file: IndexDB file not found." && exit 66
 
-    echo "Extracting downloaded boot image..."
+echo "Extracting downloaded boot image..."
 
-    BOOT=$(find $TMP -name "*.bin.zip")
+BOOT=$(find $TMP -name "*.bin.zip")
 
-    [ ! -f "$BOOT" ] && echo "Invalid PAT file: boot file not found." && exit 67
+[ ! -f "$BOOT" ] && echo "Invalid PAT file: boot file not found." && exit 67
 
-    BOOT=$(echo $BOOT | head -c -5)
-    unzip -q -o $BOOT.zip -d $TMP
+BOOT=$(echo $BOOT | head -c -5)
+unzip -q -o $BOOT.zip -d $TMP
 
-    echo "Extracting prepared disk image..."
+echo "Extracting prepared disk image..."
 
-    SYSTEM="$TMP/temp.img"
-    PLATE="/data/template.img"
+SYSTEM="$TMP/temp.img"
+PLATE="/data/template.img"
 
-    rm -f $PLATE
-    unxz $PLATE.xz
-    mv -f $PLATE $SYSTEM
+rm -f $PLATE
+unxz $PLATE.xz
+mv -f $PLATE $SYSTEM
 
-    echo "Installing system partition..."
+echo "Installing system partition..."
 
-    MOUNT="/mnt/tmp"
-    rm -rf $MOUNT && mkdir -p $MOUNT
+MOUNT="/mnt/tmp"
+rm -rf $MOUNT && mkdir -p $MOUNT
 
-    OFFSET=$(parted -s $SYSTEM unit B print | sed 's/^ //g' | grep "^1 " | tr -s ' ' | cut -d ' ' -f2 | sed 's/[^0-9]*//g')
+OFFSET=$(parted -s $SYSTEM unit B print | sed 's/^ //g' | grep "^1 " | tr -s ' ' | cut -d ' ' -f2 | sed 's/[^0-9]*//g')
 
-    if [ "$OFFSET" != "1048576" ]; then
-      echo "Invalid disk image, wrong offset: $OFFSET" && exit 68
-    fi
+if [ "$OFFSET" != "1048576" ]; then
+  echo "Invalid disk image, wrong offset: $OFFSET" && exit 68
+fi
 
-    if ! mount -t ext4 -o loop,offset=$OFFSET $SYSTEM $MOUNT ; then
-      echo "Failed to mount disk image. Docker container needs to be in privileged mode during installation." && exit 69
-    fi
+if ! mount -t ext4 -o loop,offset=$OFFSET $SYSTEM $MOUNT ; then
+  echo "Failed to mount disk image. Docker container needs to be in privileged mode during installation." && exit 69
+fi
 
-    rm -rf $MOUNT/{,.[!.],..?}*
+rm -rf $MOUNT/{,.[!.],..?}*
 
-    mv $HDA.tgz $HDA.txz
+mv $HDA.tgz $HDA.txz
 
-    tar xpfJ $HDP.txz --absolute-names -C $MOUNT/
-    tar xpfJ $HDA.txz --absolute-names -C $MOUNT/
-    tar xpfJ $IDB.txz --absolute-names -C $MOUNT/usr/syno/synoman/indexdb/
+tar xpfJ $HDP.txz --absolute-names -C $MOUNT/
+tar xpfJ $HDA.txz --absolute-names -C $MOUNT/
+tar xpfJ $IDB.txz --absolute-names -C $MOUNT/usr/syno/synoman/indexdb/
 
-    umount $MOUNT
-    rm -rf $MOUNT
+umount $MOUNT
+rm -rf $MOUNT
 
-    mv -f $BOOT $IMG/boot.img
-    mv -f $SYSTEM $IMG/system.img
+mv -f $BOOT $IMG/boot.img
+mv -f $SYSTEM $IMG/system.img
 
-    rm -rf $TMP
+rm -rf $TMP
 
 exit 0
