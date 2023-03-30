545a648cc0dcd9b62e73cd042fbf51b2b2d0b22f
Kroese <kroese@users.noreply.github.com>
2023-03-30 03:31:43 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-docker build --tag dsm .
-docker run --rm -it --name dsm --device=/dev/kvm:/dev/kvm --device=/dev/fuse:/dev/fuse --device=/dev/net/tun:/dev/net/tun --cap-add NET_ADMIN --cap-add SYS_ADMIN -p 80:5000 -p 443:5001 -p 5000:5000 -p 5001:5001 -v ${PWD}/img:/storage docker.io/library/dsm
+set -e
 
+docker build --tag dsm .
+docker images dsm:latest --format "{{.Repository}}:{{.Tag}} -> {{.Size}}"
+docker run --rm -it --name dsm --privileged --device=/dev/kvm:/dev/kvm --device=/dev/fuse:/dev/fuse --device=/dev/net/tun:/dev/net/tun --cap-add NET_ADMIN -p 80:5000 -p 443:5001 -p 5000:5000 -p 5001:5001 -v ${PWD}/img:/storage docker.io/library/dsm
