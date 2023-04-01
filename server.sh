3d9dbead11cc428f102923b2207d0f7c611cbc43
Kroese <kroese@users.noreply.github.com>
2023-04-01 22:55:07 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-HTML="<HTML><BODY><H1><CENTER>Please wait while Synology is installing...</CENTER></H1></BODY></HTML>"
+HTML="<HTML><BODY><H1><CENTER>Please wait while Virtual DSM is installing...</CENTER></H1></BODY></HTML>"
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n$HTML\r\n"
 
 while { echo -en "$RESPONSE"; } | nc -lN "${1:-8080}"; do
