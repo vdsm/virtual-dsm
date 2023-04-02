9347c90980203b2c90ddf972bf7e4026649e4770
Kroese <kroese@users.noreply.github.com>
2023-04-02 03:57:02 +0200

    Automaticly install the default packages

--- agent/agent.sh
+++ agent/agent.sh
@@ -2,11 +2,35 @@
 set -u
 
 echo "Starting agent.."
-
 chmod 666 /dev/ttyS0
 echo "Starting agent.." > /dev/ttyS0
 
-sleep 5
+first_run=false
+
+for filename in /usr/local/packages/*.spk; do
+  first_run=true
+done
+
+if [ "$first_run" = true ]; then
+
+  for filename in /usr/local/packages/*.spk; do
+    /usr/syno/bin/synopkg install $filename > /dev/null
+    rm $filename
+  done
+
+  /usr/syno/bin/synopkg start FileStation > /dev/null
+  /usr/syno/bin/synopkg start SMBService > /dev/null
+  /usr/syno/bin/synopkg start SynoFinder > /dev/null
+  /usr/syno/bin/synopkg start DhcpServer > /dev/null
+  /usr/syno/bin/synopkg start SecureSignIn > /dev/null
+  /usr/syno/bin/synopkg start Python2 > /dev/null
+  /usr/syno/bin/synopkg start ScsiTarget > /dev/null
+  /usr/syno/bin/synopkg start OAuthService > /dev/null
+
+else
+  sleep 5
+fi
+
 echo "" > /dev/ttyS0
 echo "You can now login to DSM at http://localhost:5000/" > /dev/ttyS0
 echo "" > /dev/ttyS0
