289bc91b4ec79a36539271aafb320be20989b2d1
Kroese <kroese@users.noreply.github.com>
2023-04-02 21:38:34 +0200

    Refactor codebase

---
+++ power.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -eu
+
+# Configure QEMU for graceful shutdown
+
+QEMU_MONPORT=7100
+QEMU_POWERDOWN_TIMEOUT=30
+
+_graceful_shutdown() {
+
+  local COUNT=0
+  local QEMU_MONPORT="${QEMU_MONPORT:-7100}"
+  local QEMU_POWERDOWN_TIMEOUT="${QEMU_POWERDOWN_TIMEOUT:-120}"
+
+  set +e
+  echo "Trying to shutdown gracefully.."
+
+  # Send a NMI interrupt which will be detected by the agent
+  # echo 'nmi' | nc -q 1 localhost "${QEMU_MONPORT}">/dev/null 2>&1
+
+  echo 'system_powerdown' | nc -q 1 localhost "${QEMU_MONPORT}">/dev/null 2>&1
+  echo ""
+
+  while echo 'info version'|nc -q 1 localhost "${QEMU_MONPORT:-7100}">/dev/null 2>&1 && [ "${COUNT}" -lt "${QEMU_POWERDOWN_TIMEOUT}" ]; do
+    (( COUNT++ )) || true
+    echo "Shutting down, waiting... (${COUNT}/${QEMU_POWERDOWN_TIMEOUT})"
+    sleep 1
+  done
+
+  if echo 'info version'|nc -q 1 localhost "${QEMU_MONPORT:-7100}">/dev/null 2>&1; then
+    echo "Killing the VM.."
+    echo 'quit' | nc -q 1 localhost "${QEMU_MONPORT}">/dev/null 2>&1 || true
+  fi
+
+  echo "Exiting..."
+}
+
+trap _graceful_shutdown SIGINT SIGTERM SIGHUP
+
+KVM_MON_OPTS="-monitor telnet:localhost:"${QEMU_MONPORT:-7100}",server,nowait,nodelay"
--- serial/serial.sh
+++ serial/serial.sh
@@ -2,6 +2,23 @@
 
 permanent="DSM"
 serialstart="2000"
-serialnum="$(echo "$serialstart" | tr ' ' '\n' | sort -R | tail -1)$permanent"$(printf "%06d" $((RANDOM % 30000 + 1)))
 
-echo "$serialnum"
+HOST_SERIAL=="$(echo "$serialstart" | tr ' ' '\n' | sort -R | tail -1)$permanent"$(printf "%06d" $((RANDOM % 30000 + 1)))
+GUEST_SERIAL=="$(echo "$serialstart" | tr ' ' '\n' | sort -R | tail -1)$permanent"$(printf "%06d" $((RANDOM % 30000 + 1)))
+
+./run/serial.bin -cpu=1 \
+                -buildnumber=42962 \
+                -vmmts="1679863686" \
+                -hostsn="$HOST_SERIAL" \
+                -guestsn="$GUEST_SERIAL" \
+                -vmmversion="2.6.1-12139" \
+                -cpu_arch="QEMU, Virtual CPU, X86_64" \
+                -guestuuid="ba13a19a-c0c1-4fef-9346-915ed3b98341" > /dev/null 2>&1 &
+
+KVM_SERIAL_OPTS="\
+    -serial mon:stdio \
+    -device virtio-serial-pci,id=virtio-serial0,bus=pci.0,addr=0x3 \
+    -chardev pty,id=charserial0 \
+    -device isa-serial,chardev=charserial0,id=serial0 \
+    -chardev socket,id=charchannel0,host=127.0.0.1,port=12345,reconnect=10 \
+    -device virtserialport,bus=virtio-serial0.0,nr=1,chardev=charchannel0,id=channel0,name=vchannel"
