176ed8bd951ed581f2be302e65e57e36fabf62b2
Kroese <kroese@users.noreply.github.com>
2023-04-08 01:42:02 +0200

    Quoting fix

--- serial.sh
+++ serial.sh
@@ -34,11 +34,11 @@ CPU=$(lscpu | sed -nr '/Model name/ s/.*:\s*(.*) @ .*/\1/p' | sed ':a;s/  / /;ta
 [ -z "$CPU" ] && CPU="QEMU, Virtual CPU, X86_64"
 
 ./run/serial.bin -cpu=$CPU_CORES \
-                -cpu_arch="$CPU" \
+                -cpu_arch=\"${CPU}\" \
                 -buildnumber=42962 \
                 -vmmts="1679863686" \
-                -hostsn="$HOST_SERIAL" \
-                -guestsn="$GUEST_SERIAL" \
+                -hostsn=\"${HOST_SERIAL}\" \
+                -guestsn=\"${GUEST_SERIAL}\" \
                 -vmmversion="2.6.1-12139" \
                 -guestuuid="ba13a19a-c0c1-4fef-9346-915ed3b98341" > /dev/null 2>&1 &
 
