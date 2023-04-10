07c40ada2d39f736fbb02bbe49e489c0ef1d85d0
Kroese <kroese@users.noreply.github.com>
2023-04-10 03:38:17 +0200

    Simplify

--- run.sh
+++ run.sh
@@ -34,8 +34,8 @@ fi
 [ -z "${KVM_ACC_OPTS}" ] && echo "WARNING: KVM acceleration is disabled..."
 
 EXTRA_OPTS="-nographic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
-ARGS="-m ${RAM_SIZE} -smp $CPU_CORES -machine type=q35${KVM_ACC_OPTS} ${EXTRA_OPTS} ${KVM_MON_OPTS} ${KVM_SERIAL_OPTS} ${KVM_NET_OPTS} ${KVM_DISK_OPTS}"
+ARGS="-m ${RAM_SIZE} -smp ${CPU_CORES} -machine type=q35${KVM_ACC_OPTS} ${EXTRA_OPTS} ${KVM_MON_OPTS} ${KVM_SERIAL_OPTS} ${KVM_NET_OPTS} ${KVM_DISK_OPTS}"
 
-eval "qemu-system-x86_64 ${ARGS}" &
+qemu-system-x86_64 ${ARGS} &
 
 wait $!
