7805d42a71c0378eb004318e88901e4f3f67b199
Kroese <kroese@users.noreply.github.com>
2023-04-11 21:42:23 +0200

    KVM acceleration check

--- run.sh
+++ run.sh
@@ -27,15 +27,15 @@ KVM_ACC_OPTS=""
 
 if [ -e /dev/kvm ] && sh -c 'echo -n > /dev/kvm' &> /dev/null; then
   if [[ $(grep -e vmx -e svm /proc/cpuinfo) ]]; then
-    KVM_ACC_OPTS=",accel=kvm -enable-kvm -cpu host"
+    KVM_ACC_OPTS="-machine type=q35,usb=off,accel=kvm -enable-kvm -cpu host"
   fi
 fi
 
-[ -z "${KVM_ACC_OPTS}" ] && echo "WARNING: KVM acceleration is disabled..."
+[ -z "${KVM_ACC_OPTS}" ] && echo "Error: KVM acceleration is disabled.." && exit 88
 
 RAM_SIZE=$(echo "${RAM_SIZE}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
 EXTRA_OPTS="-nographic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
-ARGS="-m ${RAM_SIZE} -smp ${CPU_CORES} -machine type=q35,usb=off${KVM_ACC_OPTS} ${EXTRA_OPTS} ${KVM_MON_OPTS} ${KVM_SERIAL_OPTS} ${KVM_NET_OPTS} ${KVM_DISK_OPTS}"
+ARGS="-m ${RAM_SIZE} -smp ${CPU_CORES} ${KVM_ACC_OPTS} ${EXTRA_OPTS} ${KVM_MON_OPTS} ${KVM_SERIAL_OPTS} ${KVM_NET_OPTS} ${KVM_DISK_OPTS}"
 
 set -m
 (
