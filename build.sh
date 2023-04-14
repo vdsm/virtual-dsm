9c1ef83aacf03b27fceeb26eab06fbb5e84415c6
Kroese <kroese@users.noreply.github.com>
2023-04-14 03:27:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 
 docker build --tag dsm .
 docker images dsm:latest --format "{{.Repository}}:{{.Tag}} -> {{.Size}}"
-docker run --rm -it --name dsm --device="/dev/kvm" --cap-add NET_ADMIN -p 80:5000 -p 443:5001 -p 5000:5000 -p 5001:5001 docker.io/library/dsm
+docker run --rm -it --name dsm -p 5000:5000  --device=/dev/kvm --cap-add NET_ADMIN --stop-timeout 60 docker.io/library/dsm
