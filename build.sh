66164e9aa647f6514748086bb305a14a85b8ead3
Kroese <kroese@users.noreply.github.com>
2023-04-16 06:39:49 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker build --tag dsm .
-docker images dsm:latest --format "{{.Repository}}:{{.Tag}} -> {{.Size}}"
-docker run --rm -it --name dsm -p 5000:5000  --device=/dev/kvm --cap-add NET_ADMIN --stop-timeout 60 docker.io/library/dsm
