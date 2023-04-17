b3c311859cd65ea1650602cb267efda52d5274d5
Kroese <kroese@users.noreply.github.com>
2023-04-17 23:01:32 +0200

    Returncodes

--- agent/service.sh
+++ agent/service.sh
@@ -6,32 +6,30 @@ SCRIPT="/usr/local/bin/agent.sh"
 status() {
   if [ -f "$PIDFILE" ]; then
     echo 'Service running'
-    exit 1
+    return 1
   fi
 }
 
 start() {
   if [ -f "$PIDFILE" ] && kill -0 "$(cat "$PIDFILE")"; then
     echo 'Service already running'
-    exit 1
+    return 1
   fi
   echo 'Starting agent service...'
   chmod 666 /dev/ttyS0
   "$SCRIPT" &> /dev/ttyS0 & echo $! > "$PIDFILE"
-  exit 0
 }
 
 stop() {
   if [ ! -f "$PIDFILE" ] || ! kill -0 "$(cat "$PIDFILE")"; then
     echo 'Service not running'
-    exit 1
+    return 1
   fi
   echo 'Stopping agent service...'
   chmod 666 /dev/ttyS0
   echo 'Stopping agent service...' > /dev/ttyS0
   kill -15 "$(cat "$PIDFILE")" && rm -f "$PIDFILE"
   echo 'Service stopped'
-  exit 0
 }
 
 case "$1" in
