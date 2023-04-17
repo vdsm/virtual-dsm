fed7268d8cca494e918da0dd35952d7192075579
Kroese <kroese@users.noreply.github.com>
2023-04-17 04:12:57 +0200

    Show curl errors

--- run/power.sh
+++ run/power.sh
@@ -42,7 +42,7 @@ _graceful_shutdown(){
   # echo 'system_powerdown' | nc -q 1 -w 1 localhost "${QEMU_MONPORT}">/dev/null
 
   # Send shutdown command to guest agent tools instead via serial port
-  RESPONSE=$(curl -s -m 2 -S http://127.0.0.1:2210/write?command=6 2>&1)
+  RESPONSE=$(curl -s -m 3 -S http://127.0.0.1:2210/write?command=6)
 
   if [[ ! "${RESPONSE}" =~ "\"success\"" ]] ; then
 
