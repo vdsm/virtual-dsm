5d48acd0663f7119bc6604a8aadd855b7448fb1f
Kroese <kroese@users.noreply.github.com>
2023-04-19 16:01:54 +0200

    smp

--- run/run.sh
+++ run/run.sh
@@ -52,7 +52,7 @@ fi
 
 RAM_SIZE=$(echo "${RAM_SIZE}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
 EXTRA_OPTS="-nographic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
-ARGS="-m ${RAM_SIZE} -smp ${CPU_CORES} ${KVM_ACC_OPTS} ${EXTRA_OPTS} ${KVM_MON_OPTS} ${KVM_SERIAL_OPTS} ${KVM_NET_OPTS} ${KVM_DISK_OPTS}"
+ARGS="-m ${RAM_SIZE} -smp ${CPU_CORES},sockets=${CPU_CORES},cores=1,threads=1 ${KVM_ACC_OPTS} ${EXTRA_OPTS} ${KVM_MON_OPTS} ${KVM_SERIAL_OPTS} ${KVM_NET_OPTS} ${KVM_DISK_OPTS}"
 
 set -m
 (
