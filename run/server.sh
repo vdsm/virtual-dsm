41e4cebf5eb64e5c42c226bc729f3cdd0f8eac2b
Kroese <kroese@users.noreply.github.com>
2023-04-20 15:06:44 +0200

    Server

--- run/server.sh
+++ run/server.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 set -eu
 
-HTML="<HTML><BODY><H1><CENTER>Please wait while Virtual DSM is installing...</CENTER></H1></BODY></HTML>"
+# Close any previous instances
+script_name=${BASH_SOURCE[0]}
+
+for pid in $(pidof -x $script_name); do
+  if [ $pid != $$ ]; then
+    kill -9 $pid 2> /dev/null
+    wait $pid 2> /dev/null
+  fi 
+done
+
+# Serve the page
+HTML="<HTML><BODY><H1><CENTER>$2</CENTER></H1></BODY></HTML>"
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n$HTML\r\n"
 
 while { echo -en "$RESPONSE"; } | nc -lN "${1:-8080}"; do
   echo "================================================"
 done
+
 
