61046d214b3e6ac9025f7a5e21a3fa40e5309481
Kroese <kroese@users.noreply.github.com>
2023-04-21 08:19:19 +0200

    Healthcheck

--- run/check.sh
+++ run/check.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
+PORT=5000
+IP="20.20.20.21"
+
+if ! curl -m 3 -ILfSs "http://$IP:$PORT/"; then
+  echo "Failed to reach $IP"
+  exit 1
+fi
+
+echo "Healthcheck OK"
 exit 0
