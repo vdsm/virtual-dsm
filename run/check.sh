b709844f221a1fb7bf3b76a4536a8a6b7c2c52fb
Kroese <kroese@users.noreply.github.com>
2023-04-21 08:30:00 +0200

    Healthcheck

--- run/check.sh
+++ run/check.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -eu
 
+# Docker Healthcheck
+
 PORT=5000
 IP="20.20.20.21"
+
+FILE="/var/dsm.ip"
+[ -f "$FILE" ] && IP=$(cat "${FILE}")
 
 if ! curl -m 3 -ILfSs "http://$IP:$PORT/"; then
   echo "Failed to reach $IP"
   exit 1
 fi
 
-echo "Healthcheck OK"
+echo "Healthcheck OK for $IP"
 exit 0
