041735694fbfbea68ece9c09fc59826b8e37d6b8
Kroese <kroese@users.noreply.github.com>
2023-04-21 08:37:39 +0200

    Healthcheck

--- run/check.sh
+++ run/check.sh
@@ -4,15 +4,19 @@ set -eu
 # Docker Healthcheck
 
 PORT=5000
-IP="20.20.20.21"
-
 FILE="/var/dsm.ip"
-[ -f "$FILE" ] && IP=$(cat "${FILE}")
 
-if ! curl -m 3 -ILfSs "http://$IP:$PORT/"; then
-  echo "Failed to reach $IP"
+if [ ! -f "${FILE}" ]; then
+  echo "IP not assigned"
   exit 1
 fi
 
-echo "Healthcheck OK for $IP"
+IP=$(cat "${FILE}")
+
+if ! curl -m 3 -ILfSs "http://${IP}:${PORT}/"; then
+  echo "Failed to reach ${IP}"
+  exit 1
+fi
+
+echo "Healthcheck OK for ${IP}"
 exit 0
