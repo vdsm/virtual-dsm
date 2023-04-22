c05b4aa0418631fd892fc3141ee7eabf391f0013
Kroese <kroese@users.noreply.github.com>
2023-04-22 20:22:38 +0200

    Mimic VMM options better

--- run/run.sh
+++ run/run.sh
@@ -56,11 +56,11 @@ if [ -z "${KVM_OPTS}" ]; then
   [ "$DEBUG" != "Y" ] && exit 88
 fi
 
-DEF_OPTS="-name dsm -nographic -nodefaults -boot strict=on -display none"
+DEF_OPTS="-nographic -nodefaults -boot strict=on -display none"
 RAM_OPTS=$(echo "-m ${RAM_SIZE}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
 CPU_OPTS="-smp ${CPU_CORES},sockets=1,dies=1,cores=${CPU_CORES},threads=1"
 KVM_OPTS="-machine type=q35,usb=off,dump-guest-core=off,hpet=off${KVM_OPTS}"
-EXTRA_OPTS="device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
+EXTRA_OPTS="-device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
 EXTRA_OPTS="$EXTRA_OPTS -object rng-random,id=objrng0,filename=/dev/urandom"
 EXTRA_OPTS="$EXTRA_OPTS -device virtio-rng-pci,rng=objrng0,id=rng0,bus=pcie.0,addr=0x1c"
 
