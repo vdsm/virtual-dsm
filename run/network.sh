4e862b5c2ff3ef0872067e9d97e3b4df30487985
Kroese <kroese@users.noreply.github.com>
2023-05-03 07:51:08 +0200

    vlan

--- run/network.sh
+++ run/network.sh
@@ -20,7 +20,7 @@ set -eu
 
 configureDHCP() {
 
-  VM_NET_VLAN="vlan"
+  VM_NET_VLAN="dsm_vlan"
   GATEWAY=$(ip r | grep default | awk '{print $3}')
   NETWORK=$(ip -o route | grep "${VM_NET_DEV}" | grep -v default | awk '{print $1}')
   IP=$(ip address show dev "${VM_NET_DEV}" | grep inet | awk '/inet / { print $2 }' | cut -f1 -d/)
