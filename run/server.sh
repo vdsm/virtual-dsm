035ecdb0e9d8eb8b8f26996c806aefeeb3870ea0
Kroese <kroese@users.noreply.github.com>
2023-05-09 02:01:38 +0200

    Generate page

--- run/server.sh
+++ run/server.sh
@@ -11,9 +11,29 @@ stop() {
 
 trap 'stop' EXIT SIGINT SIGTERM SIGHUP
 
-if [[ "$2" == "/"* ]]; then
+if [[ "$2" != "/"* ]]; then
 
-  if [[ "$2" == "/run/ip.sh" ]]; then
+  BODY="$2"
+
+  if [[ "$BODY" == "install" ]]; then
+
+    BODY="Please wait while Virtual DSM is installing...<script>setTimeout(() => { document.location.reload(); }, 9999);</script>"
+
+  fi
+
+  HTML="<!DOCTYPE html><HTML><HEAD><TITLE>VirtualDSM</TITLE><STYLE>body { color: white; background-color: #125bdb; font-family: Verdana,\
+        Arial,sans-serif; } a, a:hover, a:active, a:visited { color: white; }</STYLE></HEAD><BODY><BR><BR><H1><CENTER>$BODY</CENTER></H1></BODY></HTML>"
+
+  echo -en "HTTP/1.1 200 OK\nContent-Length: ${#HTML}\nConnection: close\n\n$HTML" > "$TMP_FILE"
+  socat TCP4-LISTEN:"${1:-5000}",reuseaddr,fork,crlf SYSTEM:"cat ${TMP_FILE}" 2> /dev/null & wait $!
+
+else
+
+  if [[ "$2" != "/run/ip.sh" ]]; then
+
+    cp "$2" "$TMP_FILE"
+
+  else
 
     { echo "#!/bin/bash"
       echo "INFO=\$(curl -s -m 5 -S http://127.0.0.1:2210/read?command=10 2>/dev/null)"
@@ -26,21 +46,9 @@ if [[ "$2" == "/"* ]]; then
       echo "echo -e \"\HTTP/1.1 200 OK\\nContent-Length: \${#HTML}\\nConnection: close\\n\\n\$HTML\""
     } > "$TMP_FILE"
 
-  else
-
-      cp "$2" "$TMP_FILE"
-
   fi
 
   chmod +x "$TMP_FILE"
   socat TCP4-LISTEN:"${1:-5000}",reuseaddr,fork,crlf SYSTEM:"$TMP_FILE" 2> /dev/null & wait $!
-
-else
-
-  HTML="<!DOCTYPE html><HTML><HEAD><TITLE>VirtualDSM</TITLE><STYLE>body { color: white; background-color: #125bdb; font-family: Verdana,\
-        Arial,sans-serif; } a, a:hover, a:active, a:visited { color: white; }</STYLE></HEAD><BODY><BR><BR><H1><CENTER>$2</CENTER></H1></BODY></HTML>"
-
-  echo -en "HTTP/1.1 200 OK\nContent-Length: ${#HTML}\nConnection: close\n\n$HTML" > "$TMP_FILE"
-  socat TCP4-LISTEN:"${1:-5000}",reuseaddr,fork,crlf SYSTEM:"cat ${TMP_FILE}" 2> /dev/null & wait $!
 
 fi
