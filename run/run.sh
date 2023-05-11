e8829310a2191a4b64a939eaa1a9d195077a9225
Kroese <kroese@users.noreply.github.com>
2023-05-11 17:23:40 +0200

    Error trap

--- run/run.sh
+++ run/run.sh
@@ -10,11 +10,11 @@ set -Eeuo pipefail
 : ${DISK_SIZE:='16G'}   # Initial data disk size
 : ${RAM_SIZE:='512M'}   # Maximum RAM amount
 
+echo "❯ Starting Virtual DSM for Docker v${VERSION}..."
+
 info () { echo -e "\E[1;34m❯ \E[1;36m$1\E[0m" ; }
 error () { echo -e >&2 "\E[1;31m❯ ERROR: $1\E[0m" ; }
-
-echo "❯ Starting Virtual DSM for Docker v${VERSION}..."
-trap 'error "Error status $? for: ${BASH_COMMAND} (line $LINENO/$BASH_LINENO)"' ERR
+trap 'error "Status $? while: ${BASH_COMMAND} (line $LINENO/$BASH_LINENO)"' ERR
 
 [ ! -f "/run/run.sh" ] && error "Script must run inside Docker container!" && exit 11
 [ "$(id -u)" -ne "0" ] && error "Script must be executed with root privileges." && exit 12
