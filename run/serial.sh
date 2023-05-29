c178c758ab77381fc071f453727e4bca09ce88fa
Kroese <kroese@users.noreply.github.com>
2023-05-29 02:10:10 +0200

    feat: Add host MAC address

--- run/serial.sh
+++ run/serial.sh
@@ -4,6 +4,7 @@ set -Eeuo pipefail
 # Docker environment variables
 
 : ${HOST_CPU:=''}
+: ${HOST_MAC:=''}
 : ${HOST_BUILD:=''}
 : ${HOST_DEBUG:=''}
 : ${HOST_SERIAL:=''}
@@ -26,9 +27,10 @@ else
 fi
 
 HOST_ARGS=()
+HOST_ARGS+=("-cpu=${CPU_CORES}")
 HOST_ARGS+=("-cpu_arch=${HOST_CPU}")
 
-[ -n "$CPU_CORES" ] && HOST_ARGS+=("-cpu=${CPU_CORES}")
+[ -n "$HOST_MAC" ] && HOST_ARGS+=("-mac=${HOST_MAC}")
 [ -n "$HOST_BUILD" ] && HOST_ARGS+=("-build=${HOST_BUILD}")
 [ -n "$HOST_SERIAL" ] && HOST_ARGS+=("-hostsn=${HOST_SERIAL}")
 [ -n "$GUEST_SERIAL" ] && HOST_ARGS+=("-guestsn=${GUEST_SERIAL}")
