fb2756c36b8edf66d9efd7f4d0f2709c9b6f94ae
Kroese <kroese@users.noreply.github.com>
2023-10-09 19:34:49 +0200

    feat: GPU passthrough support

--- run/gpu.sh
+++ run/gpu.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+[ ! -d /dev/dri ] && mkdir -m 755 /dev/dri
+
+if [ ! -c /dev/dri/card0 ]; then
+  mknod /dev/dri/card0 c 226 0
+fi
+
+if [ ! -c /dev/dri/renderD128 ]; then
+  mknod /dev/dri/renderD128 c 226 128
+fi
+
+chmod 666 /dev/dri/card0
+chmod 666 /dev/dri/renderD128
+
 DEF_OPTS="-nodefaults -boot strict=on -display egl-headless,rendernode=/dev/dri/renderD128"
 DEF_OPTS="${DEF_OPTS} -device virtio-vga,id=video0,max_outputs=1,bus=pcie.0,addr=0x1"
 
