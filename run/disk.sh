75fc1ab57003e27cbb97116bb615d06c07444e87
Kroese <kroese@users.noreply.github.com>
2023-10-10 06:39:59 +0200

    feat: Mount block device

--- run/disk.sh
+++ run/disk.sh
@@ -154,33 +154,33 @@ DISK_OPTS="\
     -drive file=${DATA},if=none,id=drive-userdata,format=raw,cache=${DISK_CACHE},aio=${DISK_IO},discard=${DISK_DISCARD},detect-zeroes=on \
     -device scsi-hd,bus=hw-userdata.0,channel=0,scsi-id=0,lun=0,drive=drive-userdata,id=userdata0,rotation_rate=${DISK_ROTATION},bootindex=3"
 
-DATA2="/storage2/data.img"
+EXTRA_DISK="/storage2/data.img"
 
-if [ -f "${DATA2}" ]; then
+if [ -f "${EXTRA_DISK}" ]; then
 
   DISK_OPTS="${DISK_OPTS} \
     -device virtio-scsi-pci,id=hw-userdata2,bus=pcie.0,addr=0xd \
-    -drive file=${DATA2},if=none,id=drive-userdata2,format=raw,cache=${DISK_CACHE},aio=${DISK_IO},discard=${DISK_DISCARD},detect-zeroes=on \
+    -drive file=${EXTRA_DISK},if=none,id=drive-userdata2,format=raw,cache=${DISK_CACHE},aio=${DISK_IO},discard=${DISK_DISCARD},detect-zeroes=on \
     -device scsi-hd,bus=hw-userdata2.0,channel=0,scsi-id=0,lun=0,drive=drive-userdata2,id=userdata2,rotation_rate=${DISK_ROTATION},bootindex=4"
 
 else
 
-  [ -d "/storage2" ] && error "Disk image ${DATA2} does not exist! Use an empty file of at least 6 GB." && exit 53
+  [ -d "$(dirname "${EXTRA_DISK}")" ] && error "Disk image ${EXTRA_DISK} does not exist! Please supply an empty file of at least 6 GB." && exit 53
 
 fi
 
-DATA3="/storage3/data.img"
+EXTRA_DISK="/storage3/data.img"
 
-if [ -f "${DATA3}" ]; then
+if [ -f "${EXTRA_DISK}" ]; then
 
   DISK_OPTS="${DISK_OPTS} \
     -device virtio-scsi-pci,id=hw-userdata3,bus=pcie.0,addr=0xe \
-    -drive file=${DATA3},if=none,id=drive-userdata3,format=raw,cache=${DISK_CACHE},aio=${DISK_IO},discard=${DISK_DISCARD},detect-zeroes=on \
+    -drive file=${EXTRA_DISK},if=none,id=drive-userdata3,format=raw,cache=${DISK_CACHE},aio=${DISK_IO},discard=${DISK_DISCARD},detect-zeroes=on \
     -device scsi-hd,bus=hw-userdata3.0,channel=0,scsi-id=0,lun=0,drive=drive-userdata3,id=userdata3,rotation_rate=${DISK_ROTATION},bootindex=5"
 
 else
 
-  [ -d "/storage3" ] && error "Disk image ${DATA3} does not exist! Use an empty file of at least 6 GB." && exit 54
+  [ -d "$(dirname "${EXTRA_DISK}")" ] && error "Disk image ${EXTRA_DISK} does not exist! Please supply an empty file of at least 6 GB." && exit 54
 
 fi
 
