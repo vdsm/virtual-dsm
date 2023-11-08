22edd1c4b5d13f77f459214f674949a83ff3173b
Kroese <kroese@users.noreply.github.com>
2023-11-08 04:21:07 +0100

    fix: Increase sleep

--- run/print.sh
+++ run/print.sh
@@ -4,22 +4,23 @@ set -Eeuo pipefail
 info () { echo -e "\E[1;34m❯\E[1;36m $1\E[0m" ; }
 error () { echo -e >&2 "\E[1;31m❯ ERROR: $1\E[0m" ; }
 
-sleep 1
 retry=true
 
 while [ "$retry" = true ]
 do
 
+  sleep 2
+  
   # Retrieve IP from guest VM
   RESPONSE=$(curl -s -m 16 -S http://127.0.0.1:2210/read?command=10 2>&1)
 
   if [[ ! "${RESPONSE}" =~ "\"success\"" ]] ; then
-    error "Failed to connect to guest: $RESPONSE" && exit 1
+    error "Failed to connect to guest: $RESPONSE" && continue
   fi
 
   # Retrieve the HTTP port number
   if [[ ! "${RESPONSE}" =~ "\"http_port\"" ]] ; then
-    error "Failed to parse response from guest: $RESPONSE" && exit 1
+    error "Failed to parse response from guest: $RESPONSE" && continue
   fi
 
   rest=${RESPONSE#*http_port}
@@ -27,11 +28,11 @@ do
   rest=${rest%%,*}
   PORT=${rest%%\"*}
 
-  [ -z "${PORT}" ] && error "Guest has not set a portnumber yet.." && sleep 3 && continue
+  [ -z "${PORT}" ] && continue
 
   # Retrieve the IP address
   if [[ ! "${RESPONSE}" =~ "eth0" ]] ; then
-    error "Failed to parse response from guest: $RESPONSE" && exit 1
+    error "Failed to parse response from guest: $RESPONSE" && continue
   fi
 
   rest=${RESPONSE#*eth0}
@@ -40,7 +41,7 @@ do
   rest=${rest#*\"}
   IP=${rest%%\"*}
 
-  [ -z "${IP}" ] && error "Guest has not received an IP yet.." && sleep 3 && continue
+  [ -z "${IP}" ] && continue
 
   retry=false
 
@@ -57,5 +58,3 @@ info "--------------------------------------------------------"
 info " You can now login to DSM at ${MSG}"
 info "--------------------------------------------------------"
 echo ""
-
-exit 0
