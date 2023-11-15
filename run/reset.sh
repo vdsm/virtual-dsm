1028e55efa6f38e8651dc4e1e2989aa9f9c9994b
Kroese <kroese@users.noreply.github.com>
2023-11-15 17:11:47 +0100

    fix: Initalization

--- run/reset.sh
+++ run/reset.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+KERNEL=$(uname -r | cut -b 1)
+MINOR=$(uname -r | cut -d '.' -f2)
+ARCH=$(dpkg --print-architecture)
+VERS=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1)
+
 rm -f /run/dsm.url
 rm -f /run/qemu.pid
 rm -f /run/qemu.count
