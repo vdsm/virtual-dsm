0cd1ddf0a14b623853f9b6eb6c38b3118d46d9d1
Kroese <kroese@users.noreply.github.com>
2023-11-15 19:58:51 +0100

    feat: Cache DSM info (#378)
    * feat: Cache location
    
    * feat: Cache location
    
    * feat: Add support URL
    
    * feat: Cache location
    
    * fix: Remove files
    
    * feat: Reset filesystem
    
    * fix: Exit when PID is missing
    
    * fix: Counter file
    
    * fix: Check flags
    
    * docs: Readme
    
    * feat: Cleanup files
    
    * fix: Check flags
    
    * fix: Check flags
    
    * fix: Initialization
    
    * fix: Initalization
    
    * fix: Initialization
    
    * fix: Cleanup temp
    
    * fix: Initialize system
    
    * feat: Config system
    
    * feat: Configure system
    
    * fix: Variables
    
    * fix: Variables
    
    * fix: Error handling
    
    * style: Comments
    
    * fix: Returnvalue
    
    * fix: Returnvalue
    
    * fix: Returnvalue
    
    * fix: Returnvalue
    
    * fix: Returnvalue
    
    * docs: Multi-disk support
    
    * feat: Use cached location
    
    * fix: Swap order

---
+++ run/config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+KVM_ERR=""
+KVM_OPTS=""
+
+if [ -e /dev/kvm ] && sh -c 'echo -n > /dev/kvm' &> /dev/null; then
+  if ! grep -q -e vmx -e svm /proc/cpuinfo; then
+    KVM_ERR="(vmx/svm disabled)"
+  fi
+else
+  [ -e /dev/kvm ] && KVM_ERR="(no write access)" || KVM_ERR="(device file missing)"
+fi
+
+if [ -n "${KVM_ERR}" ]; then
+  if [ "$ARCH" == "amd64" ]; then
+    error "KVM acceleration not detected ${KVM_ERR}, see the FAQ about this."
+    [[ "${DEBUG}" != [Yy1]* ]] && exit 88
+  fi
+else
+  KVM_OPTS=",accel=kvm -enable-kvm -cpu host"
+fi
+
+DEF_OPTS="-nographic -nodefaults -boot strict=on -display none"
+RAM_OPTS=$(echo "-m ${RAM_SIZE}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
+CPU_OPTS="-smp ${CPU_CORES},sockets=1,dies=1,cores=${CPU_CORES},threads=1"
+MAC_OPTS="-machine type=q35,usb=off,dump-guest-core=off,hpet=off${KVM_OPTS}"
+EXTRA_OPTS="-device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
+EXTRA_OPTS="$EXTRA_OPTS -object rng-random,id=objrng0,filename=/dev/urandom"
+EXTRA_OPTS="$EXTRA_OPTS -device virtio-rng-pci,rng=objrng0,id=rng0,bus=pcie.0,addr=0x1c"
+
+if [[ "${GPU}" == [Yy1]* ]] && [[ "$ARCH" == "amd64" ]]; then
+  DEF_OPTS="-nodefaults -boot strict=on -display egl-headless,rendernode=/dev/dri/renderD128"
+  DEF_OPTS="${DEF_OPTS} -device virtio-vga,id=video0,max_outputs=1,bus=pcie.0,addr=0x1"
+fi
+
+ARGS="${DEF_OPTS} ${CPU_OPTS} ${RAM_OPTS} ${MAC_OPTS} ${MON_OPTS} ${SERIAL_OPTS} ${NET_OPTS} ${DISK_OPTS} ${EXTRA_OPTS} ${ARGUMENTS}"
+ARGS=$(echo "$ARGS" | sed 's/\t/ /g' | tr -s ' ')
+
+return 0
---
+++ run/reset.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+info () { echo -e "\E[1;34m❯ \E[1;36m$1\E[0m" ; }
+error () { echo -e >&2 "\E[1;31m❯ ERROR: $1\E[0m" ; }
+trap 'error "Status $? while: ${BASH_COMMAND} (line $LINENO/$BASH_LINENO)"' ERR
+
+[ ! -f "/run/run.sh" ] && error "Script must run inside Docker container!" && exit 11
+[ "$(id -u)" -ne "0" ] && error "Script must be executed with root privileges." && exit 12
+
+# Docker environment variables
+
+: ${URL:=''}            # URL of the PAT file
+: ${GPU:='N'}           # Enable GPU passthrough
+: ${DEBUG:='N'}         # Enable debugging mode
+: ${ALLOCATE:='Y'}      # Preallocate diskspace
+: ${ARGUMENTS:=''}      # Extra QEMU parameters
+: ${CPU_CORES:='1'}     # Amount of CPU cores
+: ${DISK_SIZE:='16G'}   # Initial data disk size
+: ${RAM_SIZE:='512M'}   # Maximum RAM amount
+
+# Helper variables
+
+KERNEL=$(uname -r | cut -b 1)
+MINOR=$(uname -r | cut -d '.' -f2)
+ARCH=$(dpkg --print-architecture)
+VERS=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1)
+
+# Cleanup files
+
+rm -f /run/dsm.url
+rm -f /run/qemu.pid
+rm -f /run/qemu.count
+
+return 0
