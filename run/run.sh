d340f1fb64c86e00d69b71013c5c3c0b645afd7b
Kroese <kroese@users.noreply.github.com>
2023-11-15 18:00:16 +0100

    fix: Variables

--- run/run.sh
+++ run/run.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-# Docker environment variables
-
-: ${URL:=''}            # URL of the PAT file
-: ${GPU:='N'}           # Enable GPU passthrough
-: ${DEBUG:='N'}         # Enable debugging mode
-: ${ALLOCATE:='Y'}      # Preallocate diskspace
-: ${ARGUMENTS:=''}      # Extra QEMU parameters
-: ${CPU_CORES:='1'}     # Amount of CPU cores
-: ${DISK_SIZE:='16G'}   # Initial data disk size
-: ${RAM_SIZE:='512M'}   # Maximum RAM amount
-
 echo "❯ Starting Virtual DSM for Docker v${VERSION}..."
 echo "❯ For support visit https://github.com/vdsm/virtual-dsm/"
-
-info () { echo -e "\E[1;34m❯ \E[1;36m$1\E[0m" ; }
-error () { echo -e >&2 "\E[1;31m❯ ERROR: $1\E[0m" ; }
-trap 'error "Status $? while: ${BASH_COMMAND} (line $LINENO/$BASH_LINENO)"' ERR
-
-[ ! -f "/run/run.sh" ] && error "Script must run inside Docker container!" && exit 11
-[ "$(id -u)" -ne "0" ] && error "Script must be executed with root privileges." && exit 12
 
 . /run/reset.sh   # Initialize system
 . /run/install.sh   # Run installation
@@ -29,7 +11,7 @@ trap 'error "Status $? while: ${BASH_COMMAND} (line $LINENO/$BASH_LINENO)"' ERR
 . /run/network.sh  # Initialize network
 . /run/serial.sh   # Initialize serialport
 . /run/power.sh    # Configure shutdown
-. /run/config.sh    # Configure QEMU
+. /run/config.sh    # Configure arguments
 
 trap - ERR
 
