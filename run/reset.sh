4463a6b851e468fe2f65287b23088757ab5f4fef
Kroese <kroese@users.noreply.github.com>
2023-11-15 17:57:12 +0100

    fix: Variables

--- run/reset.sh
+++ run/reset.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# Docker environment variables
+
+: ${URL:=''}            # URL of the PAT file
+: ${GPU:='N'}           # Enable GPU passthrough
+: ${DEBUG:='N'}         # Enable debugging mode
+: ${ALLOCATE:='Y'}      # Preallocate diskspace
+: ${ARGUMENTS:=''}      # Extra QEMU parameters
+: ${CPU_CORES:='1'}     # Amount of CPU cores
+: ${DISK_SIZE:='16G'}   # Initial data disk size
+: ${RAM_SIZE:='512M'}   # Maximum RAM amount
+
 KERNEL=$(uname -r | cut -b 1)
 MINOR=$(uname -r | cut -d '.' -f2)
 ARCH=$(dpkg --print-architecture)
