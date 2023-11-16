34409ffeb47259c2e9d9ea9038d6cdf009dc46a0
Kroese <kroese@users.noreply.github.com>
2023-11-16 18:45:09 +0100

    fix: Move URL

--- src/reset.sh
+++ src/reset.sh
@@ -10,7 +10,6 @@ trap 'error "Status $? while: ${BASH_COMMAND} (line $LINENO/$BASH_LINENO)"' ERR
 
 # Docker environment variables
 
-: ${URL:=''}            # URL of the PAT file
 : ${GPU:='N'}           # Enable GPU passthrough
 : ${DEBUG:='N'}         # Enable debugging mode
 : ${ALLOCATE:='Y'}      # Preallocate diskspace
