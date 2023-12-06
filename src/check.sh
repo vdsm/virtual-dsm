1a597bf62418ac88bdf99cc4262f09abfa098a32
Kroese <kroese@users.noreply.github.com>
2023-12-06 05:25:48 +0100

    fix: Increase timeout

--- src/check.sh
+++ src/check.sh
@@ -9,7 +9,7 @@ file="/run/dsm.url"
 if [ ! -f  "$file" ]; then
 
   # Retrieve IP from guest VM for Docker healthcheck
-  RESPONSE=$(curl -s -m 16 -S http://127.0.0.1:2210/read?command=10 2>&1)
+  RESPONSE=$(curl -s -m 30 -S http://127.0.0.1:2210/read?command=10 2>&1)
 
   if [[ ! "${RESPONSE}" =~ "\"success\"" ]] ; then
     echo "Failed to connect to guest: $RESPONSE" && exit 1
