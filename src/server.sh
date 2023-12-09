e02cf1a7f401f03eccab8bb69673d6856becd586
Kroese <kroese@users.noreply.github.com>
2023-12-09 21:10:07 +0100

    fix: Curly braces

--- src/server.sh
+++ src/server.sh
@@ -14,9 +14,9 @@ trap 'stop' EXIT SIGINT SIGTERM SIGHUP
 html()
 {
     local h="<!DOCTYPE html><HTML><HEAD><TITLE>VirtualDSM</TITLE>"
-    h="${h} <STYLE>body { color: white; background-color: #125bdb; font-family: Verdana,"
-    h="${h} Arial,sans-serif; } a, a:hover, a:active, a:visited { color: white; }</STYLE></HEAD>"
-    h="${h}<BODY><BR><BR><H1><CENTER>$1</CENTER></H1></BODY></HTML>"
+    h="$h<STYLE>body { color: white; background-color: #125bdb; font-family: Verdana,"
+    h="$h Arial,sans-serif; } a, a:hover, a:active, a:visited { color: white; }</STYLE></HEAD>"
+    h="$h<BODY><BR><BR><H1><CENTER>$1</CENTER></H1></BODY></HTML>"
 
     echo "$h"
 }
@@ -46,15 +46,15 @@ if [[ "$2" != "/run/ip.sh" ]]; then
 
 else
 
-  BODY="The location of DSM is <a href='http://\${LOCATION}'>http://\${LOCATION}</a><script>"
-  BODY="${BODY}setTimeout(function(){ window.location.assign('http://\${LOCATION}'); }, 3000);</script>"
+  BODY="The location of DSM is <a href='http://\$LOCATION'>http://\$LOCATION</a><script>"
+  BODY="$BODY setTimeout(function(){ window.location.assign('http://\$LOCATION'); }, 3000);</script>"
   WAIT="Please wait while discovering IP...<script>setTimeout(() => { document.location.reload(); }, 4999);</script>"
 
   HTML=$(html "xxx")
 
   { echo "#!/bin/bash"
     echo "[ -f \"/run/dsm.url\" ] && LOCATION=\$(cat \"/run/dsm.url\")"
-    echo "HTML=\"$HTML\"; [ -z \"\${LOCATION}\" ] && BODY=\"$WAIT\" || BODY=\"$BODY\"; HTML=\${HTML/xxx/\$BODY}"
+    echo "HTML=\"$HTML\"; [ -z \"\$LOCATION\" ] && BODY=\"$WAIT\" || BODY=\"$BODY\"; HTML=\${HTML/xxx/\$BODY}"
     echo "printf '%b' \"HTTP/1.1 200 OK\\nContent-Length: \${#HTML}\\nConnection: close\\n\\n\$HTML\""
   } > "$TMP_FILE"
 
