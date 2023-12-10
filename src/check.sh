e978ec6a0fb8566e249c075eabbf2a58a1979389
Kroese <kroese@users.noreply.github.com>
2023-12-10 05:11:28 +0100

    fix: Increase timeout

--- src/check.sh
+++ src/check.sh
@@ -10,7 +10,7 @@ if [ ! -f  "$file" ]; then
 
   # Retrieve IP from guest VM for Docker healthcheck
 
-  { json=$(curl -m 30 -sk http://127.0.0.1:2210/read?command=10); rc=$?; } || :
+  { json=$(curl -m 60 -sk http://127.0.0.1:2210/read?command=10); rc=$?; } || :
   (( rc != 0 )) && echo "Failed to connect to guest: curl error $rc" && exit 1
 
   { result=$(echo "$json" | jq -r '.status'); rc=$?; } || :
