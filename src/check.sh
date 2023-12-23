b252df15fd8c320b2b55b96efd612e48a9944aaf
Kroese <kroese@users.noreply.github.com>
2023-12-23 15:19:46 +0100

    fix: Use specified LAN adaptor

--- src/check.sh
+++ src/check.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+: ${VM_NET_DEV:='eth0'}
+
 [ ! -f "/run/qemu.pid" ] && echo "QEMU not running yet.." && exit 0
 [ -f "/run/qemu.count" ] && echo "QEMU is shutting down.." && exit 1
 
@@ -17,7 +19,7 @@ if ! curl -m 20 -ILfSs "http://$location/" > /dev/null; then
     echo "Failed to reach DSM at port $port"
   else
     echo "Failed to reach DSM at http://$location"
-    ip=$(ip address show dev eth0 | grep inet | awk '/inet / { print $2 }' | cut -f1 -d/)
+    ip=$(ip address show dev "$VM_NET_DEV" | grep inet | awk '/inet / { print $2 }' | cut -f1 -d/)
   fi
 
   echo "You might need to whitelist IP $ip in the DSM firewall." && exit 1
