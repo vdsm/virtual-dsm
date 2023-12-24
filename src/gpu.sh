5e0b79edd5fba7111be34dea801f8d45981ac669
Kroese <kroese@users.noreply.github.com>
2023-12-24 14:39:06 +0100

    fix: Skip mknod errors

--- src/gpu.sh
+++ src/gpu.sh
@@ -8,15 +8,16 @@ fi
 [ ! -d /dev/dri ] && mkdir -m 755 /dev/dri
 
 if [ ! -c /dev/dri/card0 ]; then
-  mknod /dev/dri/card0 c 226 0
+  if mknod /dev/dri/card0 c 226 0; then
+    chmod 666 /dev/dri/card0
+  fi
 fi
 
 if [ ! -c /dev/dri/renderD128 ]; then
-  mknod /dev/dri/renderD128 c 226 128
+  if mknod /dev/dri/renderD128 c 226 128; then
+    chmod 666 /dev/dri/renderD128
+  fi
 fi
-
-chmod 666 /dev/dri/card0
-chmod 666 /dev/dri/renderD128
 
 addPackage "xserver-xorg-video-intel" "Intel GPU drivers"
 addPackage "qemu-system-modules-opengl" "OpenGL module"
