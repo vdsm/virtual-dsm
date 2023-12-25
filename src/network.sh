281f2992ffd4246c5b98e1ec03713ba6235dee5f
Kroese <kroese@users.noreply.github.com>
2023-12-25 04:04:01 +0100

    fix: Close file descriptors
    * fix: Close file descriptors

--- src/network.sh
+++ src/network.sh
@@ -154,6 +154,9 @@ configureNAT () {
 
 closeNetwork () {
 
+  exec 30<&- || true
+  exec 40<&- || true
+
   if [[ "$DHCP" == [Yy1]* ]]; then
 
     { pkill -f server.sh || true; } 2>/dev/null
@@ -189,7 +192,7 @@ if [ ! -c /dev/net/tun ]; then
 fi
 
 if [ ! -c /dev/net/tun ]; then
-  error "Please add the following docker settings to your container: --device=/dev/net/tun" && exit 25
+  error "Please add the following docker setting to your container: --device=/dev/net/tun" && exit 25
 fi
 
 # Create the necessary file structure for /dev/vhost-net
