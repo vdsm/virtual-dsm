c21a83e8f961614aebc9e685f7e45e2fd101119e
Kroese <kroese@users.noreply.github.com>
2023-12-28 10:13:18 +0100

    fix: Refactor

--- src/entry.sh
+++ src/entry.sh
@@ -26,28 +26,9 @@ fi
 [[ "$DEBUG" == [Yy1]* ]] && info "$VERS" && set -x
 msg=$(qemu-system-x86_64 -daemonize -pidfile "$QEMU_PID" ${ARGS:+ $ARGS})
 { set +x; } 2>/dev/null
+[[ "${msg,,}" != "char"* ||  "$msg" != *"serial0)" ]] && echo "$msg"
 
-if [[ "$msg" != "char"* ||  "$msg" != *"serial0)" ]]; then
-  echo "$msg"
-fi
-
-dev="${msg#*/dev/p}"
-dev="/dev/p${dev%% *}"
-
-if [ ! -c "$dev" ]; then
-  dev=$(echo 'info chardev' | nc -q 1 -w 1 localhost "$QEMU_PORT" | tr -d '\000')
-  dev="${dev#*charserial0}"
-  dev="${dev#*pty:}"
-  dev="${dev%%$'\n'*}"
-  dev="${dev%%$'\r'*}"
-fi
-
-if [ ! -c "$dev" ]; then
-  error "Device '$dev' not found!"
-  finish 34
-fi
-
-cat "$dev" 2>/dev/null & wait $! || true
+cat "$(terminal)" 2>/dev/null & wait $! || true
 
 sleep 1
 finish 0
