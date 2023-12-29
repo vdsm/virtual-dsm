b854aad83095385fcd4149f5fe2e84d6c759a138
Kroese <kroese@users.noreply.github.com>
2023-12-29 17:34:23 +0100

    fix: Use single IO thread (#521)

--- src/disk.sh
+++ src/disk.sh
@@ -5,10 +5,10 @@ set -Eeuo pipefail
 
 : ${DISK_IO:='native'}          # I/O Mode, can be set to 'native', 'threads' or 'io_turing'
 : ${DISK_FMT:='raw'}            # Disk file format, 'raw' by default for best performance
+: ${DISK_FLAGS:=''}             # Specifies the options for use with the qcow2 disk format
 : ${DISK_CACHE:='none'}         # Caching mode, can be set to 'writeback' for better performance
 : ${DISK_DISCARD:='on'}         # Controls whether unmap (TRIM) commands are passed to the host.
 : ${DISK_ROTATION:='1'}         # Rotation rate, set to 1 for SSD storage and increase for HDD
-: ${DISK_FLAGS:=''}             # Specifies the options for use with the qcow2 disk format
 
 BOOT="$STORAGE/$BASE.boot.img"
 SYSTEM="$STORAGE/$BASE.system.img"
@@ -17,11 +17,11 @@ SYSTEM="$STORAGE/$BASE.system.img"
 [ ! -f "$SYSTEM" ] && error "Virtual DSM system-image does not exist ($SYSTEM)" && exit 82
 
 DISK_OPTS="\
-    -object iothread,id=io1 -object iothread,id=io2 \
-    -device virtio-scsi-pci,id=hw-synoboot,iothread=io1,bus=pcie.0,addr=0xa \
+    -object iothread,id=io2 \
+    -device virtio-scsi-pci,id=hw-synoboot,iothread=io2,bus=pcie.0,addr=0xa \
     -drive file=$BOOT,if=none,id=drive-synoboot,format=raw,cache=$DISK_CACHE,aio=$DISK_IO,discard=$DISK_DISCARD,detect-zeroes=on \
     -device scsi-hd,bus=hw-synoboot.0,channel=0,scsi-id=0,lun=0,drive=drive-synoboot,id=synoboot0,rotation_rate=$DISK_ROTATION,bootindex=1 \
-    -device virtio-scsi-pci,id=hw-synosys,iothread=io1,bus=pcie.0,addr=0xb \
+    -device virtio-scsi-pci,id=hw-synosys,iothread=io2,bus=pcie.0,addr=0xb \
     -drive file=$SYSTEM,if=none,id=drive-synosys,format=raw,cache=$DISK_CACHE,aio=$DISK_IO,discard=$DISK_DISCARD,detect-zeroes=on \
     -device scsi-hd,bus=hw-synosys.0,channel=0,scsi-id=0,lun=0,drive=drive-synosys,id=synosys0,rotation_rate=$DISK_ROTATION,bootindex=2"
 
