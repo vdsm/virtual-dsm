a3d64564ed0f62f8bfc59d671fbc0cc43f3c5124
Kroese <kroese@users.noreply.github.com>
2023-12-29 16:11:30 +0100

    fix: QEMU configuration

--- src/config.sh
+++ src/config.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-DEF_OPTS="-nodefaults -boot strict=on -display none -vga none"
+DEF_OPTS="-nodefaults -boot strict=on"
 RAM_OPTS=$(echo "-m $RAM_SIZE" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
 CPU_OPTS="-cpu $CPU_MODEL -smp $CPU_CORES,sockets=1,dies=1,cores=$CPU_CORES,threads=1"
 MAC_OPTS="-machine type=q35,usb=off,dump-guest-core=off,hpet=off${KVM_OPTS}"
+DEV_OPTS="-device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
+DEV_OPTS="$DEV_OPTS -object rng-random,id=objrng0,filename=/dev/urandom"
+DEV_OPTS="$DEV_OPTS -device virtio-rng-pci,rng=objrng0,id=rng0,bus=pcie.0,addr=0x1c"
 
-EXTRA_OPTS="-device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
-EXTRA_OPTS="$EXTRA_OPTS -object rng-random,id=objrng0,filename=/dev/urandom"
-EXTRA_OPTS="$EXTRA_OPTS -device virtio-rng-pci,rng=objrng0,id=rng0,bus=pcie.0,addr=0x1c"
-[[ "$CONSOLE" != [Yy]* ]] && EXTRA_OPTS="$EXTRA_OPTS -daemonize -D $QEMU_LOG"
-
-if [[ "$GPU" == [Yy1]* ]] && [[ "$ARCH" == "amd64" ]]; then
-  DEF_OPTS="-nodefaults -boot strict=on -display egl-headless,rendernode=/dev/dri/renderD128 -vga virtio"
-fi
-
-ARGS="$DEF_OPTS $CPU_OPTS $RAM_OPTS $MAC_OPTS $MON_OPTS $SERIAL_OPTS $NET_OPTS $DISK_OPTS $EXTRA_OPTS $ARGUMENTS"
+ARGS="$DEF_OPTS $CPU_OPTS $RAM_OPTS $MAC_OPTS $DISPLAY_OPTS $MON_OPTS $SERIAL_OPTS $NET_OPTS $DISK_OPTS $DEV_OPTS $ARGUMENTS"
 ARGS=$(echo "$ARGS" | sed 's/\t/ /g' | tr -s ' ')
 
 return 0
