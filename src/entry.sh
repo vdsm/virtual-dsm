043c16d9d453407b133e3d8a1642894744301c47
Kroese <kroese@users.noreply.github.com>
2024-01-13 17:30:57 +0100

    fix: QEMU output

--- src/entry.sh
+++ src/entry.sh
@@ -22,11 +22,12 @@ if [[ "$CONSOLE" == [Yy]* ]]; then
   exec qemu-system-x86_64 ${ARGS:+ $ARGS}
 fi
 
-[[ "$DEBUG" == [Yy1]* ]] && info "$VERS" && echo "Arguments: $ARGS"
+[[ "$DEBUG" == [Yy1]* ]] && info "$VERS" && echo "Arguments: $ARGS" && echo
 { qemu-system-x86_64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
 (( rc != 0 )) && error "$(cat "$QEMU_LOG")" && exit 15
 
+terminal
 tail -fn +0 "$QEMU_LOG" 2>/dev/null &
-terminal && cat "$QEMU_TERM" 2>/dev/null & wait $! || :
+cat "$QEMU_TERM" 2>/dev/null & wait $! || :
 
 sleep 1 && finish 0
