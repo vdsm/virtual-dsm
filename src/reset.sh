4646c38554c370e8d004ac8f9f7aabb5afdbdd29
Kroese <kroese@users.noreply.github.com>
2024-01-19 15:10:30 +0100

    feat: Add warning macro

--- src/reset.sh
+++ src/reset.sh
@@ -3,6 +3,7 @@ set -Eeuo pipefail
 
 info () { printf "%b%s%b" "\E[1;34m❯ \E[1;36m" "$1" "\E[0m\n"; }
 error () { printf "%b%s%b" "\E[1;31m❯ " "ERROR: $1" "\E[0m\n" >&2; }
+warn () { printf "%b%s%b" "\E[1;31m❯ " "Warning: $1" "\E[0m\n" >&2; }
 
 trap 'error "Status $? while: $BASH_COMMAND (line $LINENO/$BASH_LINENO)"' ERR
 
