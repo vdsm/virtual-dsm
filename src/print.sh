1f3060cf76ffde7e8681be013138e6e9cf234400
Kroese <kroese@users.noreply.github.com>
2024-01-20 18:54:36 +0100

    feat: HTML template

--- src/print.sh
+++ src/print.sh
@@ -7,8 +7,10 @@ info () { printf "%b%s%b" "\E[1;34m❯ \E[1;36m" "$1" "\E[0m\n" >&2; }
 error () { printf "%b%s%b" "\E[1;31m❯ " "ERROR: $1" "\E[0m\n" >&2; }
 
 file="/run/shm/dsm.url"
+page="/run/shm/index.html"
 address="/run/shm/qemu.ip"
 shutdown="/run/shm/qemu.end"
+template="/var/www/index.html"
 url="http://127.0.0.1:2210/read?command=10"
 
 resp_err="Guest returned an invalid response:"
@@ -66,6 +68,18 @@ location=$(<"$file")
 if [[ "$location" != "20.20"* ]]; then
 
   msg="http://$location"
+  title="<title>Virtual DSM</title>"
+  body="The location of DSM is <a href='http://\$location'>http://\$location</a>"
+  script="<script>setTimeout(function(){ window.location.assign('http://\$location'); }, 3000);</script>"
+
+  HTML=$(<"$template")
+  HTML="${HTML/\[1\]/$title}"
+  HTML="${HTML/\[2\]/$script}"
+  HTML="${HTML/\[3\]/$body}"
+  HTML="${HTML/\[4\]/}"
+  HTML="${HTML/\[5\]/}"
+
+  echo "$HTML" > "$page"
 
 else
 
