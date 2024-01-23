9cbb51cc867531192f9d2a0f9ca983ede3215635
Kroese <kroese@users.noreply.github.com>
2024-01-23 19:29:07 +0100

    feat: Display QEMU version (#593)

--- src/entry.sh
+++ src/entry.sh
@@ -18,11 +18,13 @@ cd /run
 
 trap - ERR
 
+info "Booting $APP using $VERS..."
+
 if [[ "$CONSOLE" == [Yy]* ]]; then
   exec qemu-system-x86_64 ${ARGS:+ $ARGS}
 fi
 
-[[ "$DEBUG" == [Yy1]* ]] && info "$VERS" && echo "Arguments: $ARGS" && echo
+[[ "$DEBUG" == [Yy1]* ]] && echo "Arguments: $ARGS" && echo
 { qemu-system-x86_64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
 (( rc != 0 )) && error "$(<"$QEMU_LOG")" && exit 15
 
