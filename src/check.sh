437e4cc102e63bccb13484f9264f9b19f7350f04
Kroese <kroese@users.noreply.github.com>
2024-03-11 14:04:10 +0100

    feat: Add option to disable networking

--- src/check.sh
+++ src/check.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+: "${NETWORK:="Y"}"
+
 [ -f "/run/shm/qemu.end" ] && echo "QEMU is shutting down.." && exit 1
 [ ! -f "/run/shm/qemu.pid" ] && echo "QEMU is not running yet.." && exit 0
 [[ "$NETWORK" != [Yy1]* ]] && echo "Networking is disabled.." && exit 0
