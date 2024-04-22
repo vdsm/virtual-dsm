6e8900edfccfe2432b49aea695b650fff3b4f7c5
Kroese <kroese@users.noreply.github.com>
2024-04-22 16:44:48 +0200

    fix: CPU detection

--- src/reset.sh
+++ src/reset.sh
@@ -43,6 +43,7 @@ KERNEL=$(uname -r | cut -b 1)
 MINOR=$(uname -r | cut -d '.' -f2)
 ARCH=$(dpkg --print-architecture)
 VERS=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
+CPU=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1' | sed 's# @.*##g' | sed s/"(R)"//g | sed 's/[^[:alnum:] ]\+/ /g' | sed 's/  */ /g')
 
 # Check system
 
