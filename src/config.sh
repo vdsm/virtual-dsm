ace6f76aadcd4b7a323d86e2c27ea4c69bbee5e2
Kroese <kroese@users.noreply.github.com>
2024-05-02 20:12:34 +0200

    Update config.sh

--- src/config.sh
+++ src/config.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 DEF_OPTS="-nodefaults -boot strict=on"
-RAM_OPTS=$(echo "-m $RAM_SIZE" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
+RAM_OPTS=$(echo "-m ${RAM_SIZE^^}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
 CPU_OPTS="-cpu $CPU_FLAGS -smp $CPU_CORES,sockets=1,dies=1,cores=$CPU_CORES,threads=1"
 MAC_OPTS="-machine type=q35,usb=off,vmport=off,dump-guest-core=off,hpet=off${KVM_OPTS}"
 DEV_OPTS="-device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4"
