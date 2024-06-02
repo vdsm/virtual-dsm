19b42489290a785b854d7fbc0fd75216b22a1626
Kroese <kroese@users.noreply.github.com>
2024-06-02 21:04:09 +0200

    feat: Print QEMU version on boot (#751)

--- src/entry.sh
+++ src/entry.sh
@@ -18,7 +18,8 @@ cd /run
 
 trap - ERR
 
-info "Booting ${APP}..."
+version=$(qemu-system-aarch64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
+info "Booting $APP using QEMU v$version..."
 
 if [[ "$CONSOLE" == [Yy]* ]]; then
   exec qemu-system-x86_64 ${ARGS:+ $ARGS}
