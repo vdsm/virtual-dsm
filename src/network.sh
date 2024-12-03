d078af0397125ea86c05e5278c6512bf71a87d13
Kroese <kroese@users.noreply.github.com>
2024-12-03 11:50:56 +0100

    fix: TUN device error (#863)

--- src/network.sh
+++ src/network.sh
@@ -168,6 +168,9 @@ configureUser() {
 
 configureNAT() {
 
+  local tuntap="TUN device is missing. $ADD_ERR --device /dev/net/tun"
+  local tables="The 'ip_tables' kernel module is not loaded. Try this command: sudo modprobe ip_tables iptable_nat"
+
   # Create the necessary file structure for /dev/net/tun
   if [ ! -c /dev/net/tun ]; then
     [ ! -d /dev/net ] && mkdir -m 755 /dev/net
@@ -177,7 +180,7 @@ configureNAT() {
   fi
 
   if [ ! -c /dev/net/tun ]; then
-    error "TUN device missing. $ADD_ERR --device /dev/net/tun --cap-add NET_ADMIN" && return 1
+    error "$tuntap" && return 1
   fi
 
   # Check port forwarding flag
@@ -187,9 +190,6 @@ configureNAT() {
       error "IP forwarding is disabled. $ADD_ERR --sysctl net.ipv4.ip_forward=1" && return 1
     fi
   fi
-
-  local tables="The 'ip_tables' kernel module is not loaded. Try this command: sudo modprobe ip_tables iptable_nat"
-  local tuntap="The 'tun' kernel module is not available. Try this command: 'sudo modprobe tun' or run the container with 'privileged: true'."
 
   # Create a bridge with a static IP for the VM guest
 
