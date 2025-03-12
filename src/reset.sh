c9a5acb6f7a36131b705693842479e077586b1bb
Kroese <kroese@users.noreply.github.com>
2025-03-12 21:13:27 +0100

    Update reset.sh

--- src/reset.sh
+++ src/reset.sh
@@ -108,7 +108,11 @@ RAM_SPARE=500000000
 RAM_AVAIL=$(free -b | grep -m 1 Mem: | awk '{print $7}')
 RAM_TOTAL=$(free -b | grep -m 1 Mem: | awk '{print $2}')
 RAM_SIZE=$(echo "${RAM_SIZE^^}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
+[[ -z ${RAM_SIZE//[0-9]} ]] && [ "$RAM_SIZE" -lt "130" ] && RAM_SIZE="${RAM_SIZE}G"
+! numfmt --from=iec "$RAM_SIZE" &>/dev/null && error "Invalid RAM size: $RAM_SIZE" && exit 15
 RAM_WANTED=$(numfmt --from=iec "$RAM_SIZE")
+[ "$RAM_WANTED" -lt "136314880 " ] && error "Invalid RAM size: $RAM_SIZE" && exit 16
+
 AVAIL_GB=$(( RAM_AVAIL/1073741824 ))
 TOTAL_GB=$(( (RAM_TOTAL + 1073741823)/1073741824 ))
 WANTED_GB=$(( (RAM_WANTED + 1073741823)/1073741824 ))
