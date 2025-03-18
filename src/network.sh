336a0e4907653a3e1ef142fd129b5552974f9cb3
Kroese <kroese@users.noreply.github.com>
2025-03-18 19:24:28 +0100

    Update network.sh

--- src/network.sh
+++ src/network.sh
@@ -442,7 +442,10 @@ if [[ "$DEBUG" == [Yy1]* ]]; then
   line="Host: $HOST  IP: $IP  Gateway: $GATEWAY  Interface: $VM_NET_DEV  MAC: $VM_NET_MAC  MTU: $mtu"
   [[ "$MTU" != "0" ]] && [[ "$MTU" != "$mtu" ]] && line+=" ($MTU)"
   info "$line"
-  [ -f /etc/resolv.conf ] && grep '^nameserver*' /etc/resolv.conf
+  if [ -f /etc/resolv.conf ]; then
+    nameservers=$(grep '^nameserver*' /etc/resolv.conf | head -c -1 | sed 's/nameserver //g;' | sed -z 's/\n/, /g')
+    [ -n "$nameservers" ] && info "Nameservers: $nameservers"
+  fi
   echo
 fi
 
