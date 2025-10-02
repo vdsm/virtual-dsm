06d0ab35f4e79e3ac85fb10d842c5586c1c12578
Kroese <kroese@users.noreply.github.com>
2025-10-02 17:15:18 +0200

    feat: Add debug trace option

--- src/reset.sh
+++ src/reset.sh
@@ -2,6 +2,7 @@
 set -Eeuo pipefail
 
 trap 'error "Status $? while: $BASH_COMMAND (line $LINENO/$BASH_LINENO)"' ERR
+[[ "${TRACE:-}" == [Yy1]* ]] && set -o functrace && trap 'echo "# $BASH_COMMAND" >&2' DEBUG
 
 [ ! -f "/run/entry.sh" ] && error "Script must run inside Docker container!" && exit 11
 [ "$(id -u)" -ne "0" ] && error "Script must be executed with root privileges." && exit 12
