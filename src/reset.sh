e48e937cc12492cde4f609dae685269b58d5124f
Kroese <kroese@users.noreply.github.com>
2025-10-08 13:41:10 +0200

    Cleanup reset.sh
    Removed unused port variable assignments and webserver startup logic.

--- src/reset.sh
+++ src/reset.sh
@@ -206,32 +206,6 @@ rm -f /run/shm/dsm.url
 rm -rf /tmp/dsm
 rm -rf "$STORAGE/tmp"
 
-
-: "${COM_PORT:="2210"}"    # Comm port
-: "${MON_PORT:="7100"}"    # Monitor port
-: "${WEB_PORT:="5000"}"    # Webserver port
-: "${CHR_PORT:="12345"}"   # Character port
-
-cp -r /var/www/* /run/shm
 html "Starting $APP for $ENGINE..."
-
-if [[ "${WEB:-}" != [Nn]* ]]; then
-
-  mkdir -p /etc/nginx/sites-enabled
-  cp /etc/nginx/default.conf /etc/nginx/sites-enabled/web.conf
-
-  sed -i "s/listen 5000 default_server;/listen $WEB_PORT default_server;/g" /etc/nginx/sites-enabled/web.conf
-  
-  # shellcheck disable=SC2143
-  if [ -f /proc/net/if_inet6 ] && [ -n "$(ifconfig -a | grep inet6)" ]; then
-
-    sed -i "s/listen $WEB_PORT default_server;/listen [::]:$WEB_PORT default_server ipv6only=off;/g" /etc/nginx/sites-enabled/web.conf
-
-  fi
-  
-  # Start webserver
-  nginx -e stderr
-
-fi
 
 return 0
