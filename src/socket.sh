3db91f077fd76a2e0132625b2bbc90f996e96690
Kroese <kroese@users.noreply.github.com>
2025-10-11 17:56:44 +0200

    fix: Relay last status message (#1054)

--- src/socket.sh
+++ src/socket.sh
@@ -3,9 +3,14 @@ set -Eeuo pipefail
 
 path="/run/shm/msg.html"
 
+if [ -f "$path" ] && [ -s "$path" ]; then
+  echo -n "s: " && cat "$path"
+fi
+
 inotifywait -m "$path" |
   while read -r fp event fn; do
     case "${event,,}" in
-      "modify" ) echo -n "s: " && cat "$path" ;;
+      "modify"* ) echo -n "s: " && cat "$path" ;;
+      "delete_self" ) echo "c: vnc" ;;
     esac    
   done
