d068a318aa248922c9f0be34538a517121b2a1c6
Kroese <kroese@users.noreply.github.com>
2025-10-16 12:35:01 +0200

    Update config.sh

--- src/config.sh
+++ src/config.sh
@@ -14,26 +14,86 @@ ARGS=$(echo "$ARGS" | sed 's/\t/ /g' | tr -s ' ')
 
 # Check available memory as the very last step
 
-if [[ "$RAM_CHECK" != [Nn]* ]]; then
+RAM_AVAIL=$(free -b | grep -m 1 Mem: | awk '{print $7}')
+  
+if [[ "$RAM_CHECK" != [Nn]* && "${RAM_SIZE,,}" != "max" && "${RAM_SIZE,,}" != "half" ]]; then
 
-  RAM_AVAIL=$(free -b | grep -m 1 Mem: | awk '{print $7}')
   AVAIL_MEM=$(formatBytes "$RAM_AVAIL")
 
   if (( (RAM_WANTED + RAM_SPARE) > RAM_AVAIL )); then
-    msg="Your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is too high for the $AVAIL_MEM of memory available, please set a lower value."
-    [[ "${FS,,}" != "zfs" ]] && error "$msg" && exit 17
-    info "$msg"
+    msg="Your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is too high for the $AVAIL_MEM of memory available,"
+    if [[ "${FS,,}" == "zfs" ]]; then
+      info "$msg but since ZFS is active this will be ignored."
+    else
+      RAM_SIZE="max"
+      warn "$msg it will automatically be adjusted to a lower amount."
+    fi
   else
     if (( (RAM_WANTED + (RAM_SPARE * 3)) > RAM_AVAIL )); then
-      msg="your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is very close to the $AVAIL_MEM of memory available, please consider a lower value."
-      if [[ "${FS,,}" != "zfs" ]]; then
-        warn "$msg"
+      msg="your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is very close to the $AVAIL_MEM of memory available,"
+      if [[ "${FS,,}" == "zfs" ]]; then
+        info "$msg but since ZFS is active this will be ignored."
       else
-        info "$msg"
+        warn "$msg please consider a lower amount."
       fi
     fi
   fi
 
+fi
+
+if [[ "${RAM_SIZE,,}" == "half" ]]; then
+
+  RAM_WANTED=$(( RAM_AVAIL / 2 ))
+  RAM_WANTED=$(( RAM_WANTED / 1073741825 ))
+
+  if (( "$RAM_WANTED" < 1 )); then
+    RAM_WANTED=$(( RAM_AVAIL / 2 ))
+    RAM_WANTED=$(( RAM_WANTED / 1048577 ))
+    RAM_SIZE="${RAM_WANTED}M"
+  else
+    RAM_SIZE="${RAM_WANTED}G"
+  fi
+
+fi
+
+if [[ "${RAM_SIZE,,}" == "max" ]]; then
+
+  RAM_WANTED=$(( RAM_AVAIL - (RAM_SPARE * 3) ))
+  RAM_WANTED=$(( RAM_WANTED / 1073741825 ))
+
+  if (( "$RAM_WANTED" < 1 )); then
+
+    RAM_WANTED=$(( RAM_AVAIL - (RAM_SPARE * 2) ))
+    RAM_WANTED=$(( RAM_WANTED / 1073741825 ))
+
+    if (( "$RAM_WANTED" < 1 )); then
+
+      RAM_WANTED=$(( RAM_AVAIL - RAM_SPARE ))
+      RAM_WANTED=$(( RAM_WANTED / 1073741825 ))
+
+      if (( "$RAM_WANTED" < 1 )); then
+  
+        RAM_WANTED=$(( RAM_AVAIL - RAM_SPARE ))
+        RAM_WANTED=$(( RAM_WANTED / 1048577 ))
+  
+        if (( "$RAM_WANTED" < 1 )); then
+
+          RAM_WANTED=$(( RAM_AVAIL ))
+          RAM_WANTED=$(( RAM_WANTED / 1048577 ))
+            
+        fi
+
+        RAM_SIZE="${RAM_WANTED}M"
+      else
+        RAM_SIZE="${RAM_WANTED}G"
+      fi
+    else
+      RAM_SIZE="${RAM_WANTED}G"
+    fi
+  else
+    RAM_SIZE="${RAM_WANTED}G"
+  fi
+  
 fi
 
 if [[ "$DEBUG" == [Yy1]* ]]; then
