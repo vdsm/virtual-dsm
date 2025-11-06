af5cd10e3f699241736386e78b5662c74ee84e39
Kroese <kroese@users.noreply.github.com>
2025-11-06 03:35:06 +0100

    Update reset.sh

--- src/reset.sh
+++ src/reset.sh
@@ -31,8 +31,8 @@ PROCESS="${APP,,}"
 PROCESS="${PROCESS// /-}"
 
 if [ -f "/run/.containerenv" ]; then
-  ENGINE="${CONTAINER:-}"
-  if [[ "${ENGINE,,}" == "podman"* ]]; then
+  ENGINE="${container:-}"
+  if [[ "${ENGINE,,}" == *"podman"* ]]; then
     ROOTLESS="Y"
     ENGINE="Podman"
   else
