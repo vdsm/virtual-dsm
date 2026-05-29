3f8b10798734063dbc082d746672cb35f44c69d3
Kroese <kroese@users.noreply.github.com>
2026-05-29 17:01:39 +0200

    fix: IP address parsing

--- src/network.sh
+++ src/network.sh
@@ -463,7 +463,7 @@ configureNAT() {
   fi
 
   local ip base gateway exclude
-  base=$(echo "$IP" | sed -r 's/([^.]*.){2}//')
+  base=$(cut -d. -f3,4 <<< "$IP")
 
   if [[ "$IP" != "172.30."* ]]; then
     ip="172.30.$base"
