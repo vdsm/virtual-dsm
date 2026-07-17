7a15eea736a5f7eb4bc3d864ff59f14a1c8253a8
Kroese <kroese@users.noreply.github.com>
2026-07-17 16:25:56 +0200

    Update utils.sh

--- src/utils.sh
+++ src/utils.sh
@@ -7,6 +7,14 @@ info () { printf "%b%s%b" "\E[1;34m❯ \E[1;36m" "${1:-}" "\E[0m\n"; }
 error () { printf "%b%s%b" "\E[1;31m❯ " "ERROR: ${1:-}" "\E[0m\n" >&2; }
 warn () { printf "%b%s%b" "\E[1;31m❯ " "Warning: ${1:-}" "\E[0m\n" >&2; }
 
+interactive() {
+
+  [ -t 1 ] &&
+    [ -c /dev/tty ] &&
+    : 2>/dev/null </dev/tty >/dev/tty
+
+}
+
 strip() {
 
   local value="${1:-}"
