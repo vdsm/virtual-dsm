4544620d4810452b7199910def00183c30232b27
Kroese <kroese@users.noreply.github.com>
2026-08-01 05:49:50 +0200

    feat: Increase indentation of printed QEMU arguments (#1347)

--- src/finish.sh
+++ src/finish.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 if enabled "$DEBUG"; then
-  printf "QEMU arguments:\n\n%s\n\n" "${ARGS// -/$'\n-'}"
+  printf "QEMU arguments:\n\n    %s\n\n" "${ARGS// -/$'\n    -'}"
 fi
 
 return 0
