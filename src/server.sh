3fb9e1e0d0f5a77484c4215f583792d2e818e3a3
Kroese <kroese@users.noreply.github.com>
2026-08-07 01:58:42 +0200

    Update server.sh

--- src/server.sh
+++ src/server.sh
@@ -55,10 +55,7 @@ configureNginx() {
 
   rm -f /etc/nginx/sites-enabled/default || return 1
 
-  # TODO: Use setfacl to grant www-data access to QEMU's Unix socket
-  # and restore unprivileged nginx workers.
   if ! sed -i \
-    -e 's/^user .*/user root;/' \
     -e 's/^worker_processes.*/worker_processes 1;/' \
     /etc/nginx/nginx.conf; then
     error "Failed to configure nginx!"
