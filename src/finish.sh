b820c54d8d9410ca99cf6d6debb2919b45ef214d
Kroese <kroese@users.noreply.github.com>
2026-08-14 13:26:42 +0200

    Update finish.sh

--- src/finish.sh
+++ src/finish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+fKill "progress.sh"
+
 if [ -s "$QEMU_DIR/qemu.host" ] && [[ "$(<"$QEMU_DIR/qemu.host")" == "172.17."* ]]; then
   warn "your container IP starts with 172.17.* which will cause conflicts when you install the Container Manager package inside DSM!"
 fi
